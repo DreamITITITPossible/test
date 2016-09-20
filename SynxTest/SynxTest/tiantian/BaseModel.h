a5877f345c16f8b8005c4a8ca351fc9a858f8817
Jiang <suinian0531@163.com>
2016-09-20 17:24:25 +0800

    test

---
+++ SynxTest/SynxTest/tiantian/BaseModel.h
@@ -0,0 +1,13 @@
+//
+//  BaseModel.h
+//  SynxTest
+//
+//  Created by dllo on 16/9/20.
+//  Copyright © 2016年 dllo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaseModel : NSObject
+
+@end
---
+++ SynxTest/SynxTest/tiantian/BaseModel.m
@@ -0,0 +1,13 @@
+//
+//  BaseModel.m
+//  SynxTest
+//
+//  Created by dllo on 16/9/20.
+//  Copyright © 2016年 dllo. All rights reserved.
+//
+
+#import "BaseModel.h"
+
+@implementation BaseModel
+
+@end
